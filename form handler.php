2656b87a38d8ce83c7c0da54dde2d29e23096cd6
Clinton Owino <clintnowino@gmail.com>
2025-06-17 12:43:04 +0300

    Updates

--- form handler.php
+++ form handler.php
@@ -1,25 +1,36 @@
 <?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
+// Basic validation
+if (isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])) {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $visitor_email = filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL);
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
 
-$email_from = 'designerclint5@gmail.com';
-$email_subject ='New Form Submission';
-$email_body ="User Name: $name.\n".
-           "User Email: $visitor_email.\n".
-           "Subject: $subject.\n".
-           "User Message:$message.\n".
+    if ($name && $visitor_email && $subject && $message) {
+        $email_from = 'designerclint5@gmail.com';
+        $email_subject = 'New Form Submission';
+        $email_body = "User Name: $name.\n" .
+            "User Email: $visitor_email.\n" .
+            "Subject: $subject.\n" .
+            "User Message: $message.\n";
 
-$to ='clintnowino@gmail.com';
-$headers = "From:$email_from \r\n";
-$headers = "Reply-To: $visitor_email \r\n";
+        $to = 'clintnowino@gmail.com';
+        $headers = "From: $email_from\r\n";
+        $headers .= "Reply-To: $visitor_email\r\n";
 
-mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html");
-
-
-
-
-
+        if (mail($to, $email_subject, $email_body, $headers)) {
+            header("Location: contact.html?success=1");
+            exit();
+        } else {
+            header("Location: contact.html?error=mailfail");
+            exit();
+        }
+    } else {
+        header("Location: contact.html?error=invalid");
+        exit();
+    }
+} else {
+    header("Location: contact.html?error=missing");
+    exit();
+}
 ?>
